f6c3084641994bb2f9626fd112df2f4e04d1c797
Alonso del Arte <alonso.delarte@gmail.com>
2024-08-22 20:16:18 -0400

    Passing constructor rejects null money amount test

--- src/main/java/com/bankingapplication/entity/Transaction.java
+++ src/main/java/com/bankingapplication/entity/Transaction.java
@@ -8,7 +8,10 @@ import org.joda.money.Money;
 public abstract class Transaction {
 
     public Transaction(Money amount, LocalDateTime date) {
-        //
+        if (amount == null) {
+            String excMsg = "Money amount should not be null";
+            throw new NullPointerException(excMsg);
+        }
     }
 
 }
